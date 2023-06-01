ba8f2b22e2cce436cc0f21c863efd6bfe3845058
lts137216 <19522133@gm.uit.edu.vn>
2023-06-01 23:58:26 +0700

    format data crawl

--- axiosServer.js
+++ axiosServer.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 const workbook = new Excel.Workbook();
 const worksheet = workbook.addWorksheet('data');
 
-const dataColumns = [{ key: 'content', header: 'Content' }];
+const dataColumns = [ { key: 'stt', header: 'STT' }, { key: 'content', header: 'Content' }];
 
 worksheet.columns = dataColumns;
 
@@ -24,38 +24,70 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-const convertData = (rawData) => {
-  const convertedData = {
-    '1D': rawData[0].toFixed(2),
-    '1W': rawData[1].toFixed(2),
-    '1M': rawData[2].toFixed(2),
-    '1Y': rawData[3].toFixed(2),
-  };
-  return convertedData;
-};
+const itemInfo = [
+  {
+    itemId: '3015403040',
+    limit: 30,
+    offset: 0,
+    shopId: '40342563',
+    type: 0
+  },
+  {
+    itemId: '11289182012',
+    limit: 30,
+    offset: 0,
+    shopId: '448978223',
+    type: 1
+  }
+]
+
+const formatComment = (comment) => {
+  return comment.replace(/\s+/g, ' ').trim()
+}
+
 
 const crawlData = async () => {
-  const url = `https://shopee.vn/api/v2/item/get_ratings?exclude_filter=0&filter=0&filter_size=0&flag=1&fold_filter=0&itemid=3015403040&limit=30&offset=0&relevant_reviews=false&request_source=1&shopid=40342563&tag_filter=&type=0&variation_filters=`;
+  const ratings = []
 
-  const data = await axios.get(url);
+  await Promise.all(itemInfo.map(async (item, index) => {
+    const url = `
+    https://shopee.vn/api/v2/item/get_ratings?exclude_filter=0&filter=0&filter_size=0&flag=1&fold_filter=0&itemid=${item.itemId}&limit=${item.limit}&offset=${item.offset}&relevant_reviews=false&request_source=1&shopid=${item.shopId}&tag_filter=&type=${item.type}&variation_filters=`
 
-  data.data.data.ratings.map(async (rating) => {
-    if (rating.comment.split('\n')[4]) {
-      worksheet.addRow({ content: rating.comment.split('\n')[4] });
-    } else if (rating.comment.split('\n')[3]) {
-      worksheet.addRow({ content: rating.comment.split('\n')[3] });
-    } else if (rating.comment.split('\n')[2]) {
-      worksheet.addRow({ content: rating.comment.split('\n')[2] });
-    } else if (rating.comment.split('\n')[1]) {
-      worksheet.addRow({ content: rating.comment.split('\n')[1] });
-    } else if (rating.comment.split('\n')[0]) {
-      worksheet.addRow({ content: rating.comment.split('\n')[0] });
-    }
-  });
+    const response = await axios.get(url);
+    ratings.push(...response.data.data.ratings);
+  }))
+
+  return ratings
 };
 
+const renderColumnData = (index, comment) => {
+  return {
+    stt: index,
+    content: formatComment(comment)
+  }
+}
+
+const addDataToWorkSheet = async () => {
+  const ratings = await crawlData()
+
+  ratings.forEach((rating, index) => {
+    if (rating.comment.split('\n')[4]) {
+      worksheet.addRow(renderColumnData(index, rating.comment.split('\n')[4]));
+    } else if (rating.comment.split('\n')[3]) {
+      worksheet.addRow(renderColumnData(index, rating.comment.split('\n')[3]));
+    } else if (rating.comment.split('\n')[2]) {
+      worksheet.addRow(renderColumnData(index, rating.comment.split('\n')[2]));
+    } else if (rating.comment.split('\n')[1]) {
+      worksheet.addRow(renderColumnData(index, rating.comment.split('\n')[1]));
+    } else if (rating.comment.split('\n')[0]) {
+      worksheet.addRow(renderColumnData(index, rating.comment.split('\n')[0]));
+    }
+  });
+}
+
 const writeToExcel = async () => {
-  await crawlData();
+  await addDataToWorkSheet()
+
   const exportPath = path.resolve(__dirname, 'countries.xlsx');
 
   await workbook.xlsx.writeFile(exportPath);
